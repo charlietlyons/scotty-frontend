78c833b1fe64df1171dcd3e77e946efb4a675fc1
Charlie Lyons <charlietlyons@gmail.com>
2023-04-24 15:58:45 -0500

    Set data on view

--- src/client/backendClient.js
+++ src/client/backendClient.js
@@ -1,14 +1,13 @@
 const URL = PRODUCTION ? "http://localhost:5000" : "http://localhost:5000";
 
 class backendClient {
-  static getProductDetailsById(productId, setProductInfo) {
-    console.log(`${URL}/inventory/${productId}`);
+  static getProductDetailsById = (productId, setProductDetailInfo) => {
     fetch(`${URL}/inventory/${productId}`, {})
       .then((response) => {
         return response.json();
       })
       .then((productData) => {
-        setProductInfo(productData);
+        setProductDetailInfo(productData);
       });
   }
 
--- src/product-details/ProductDetails.js
+++ src/product-details/ProductDetails.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import backendClient from "../client/backendClient";
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { productInfo, setProductInfo } = useState([]);
+  const [productInfo, setProductInfo] = useState({});
 
-  console.log(backendClient.getProductDetailsById(id, setProductInfo));
-  return <>{productInfo}</>;
+  useEffect(() => {
+    backendClient.getProductDetailsById(id, (data) => {
+      setProductInfo(data)
+    });
+  }, [setProductInfo]);
+
+  return <div>
+      <h1>Product: {productInfo.name}</h1>
+      <h2>Price: {productInfo.price}</h2>
+      <h2>Size: {productInfo.size}</h2>
+      <h2>Quantity: {productInfo.quantity}</h2>
+      <h2>Image URL: {productInfo.image_url}</h2>
+      <p>Description: {productInfo.description}</p>
+    </div>;
 };
 
 export default ProductDetails;
