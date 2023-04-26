9541edc8fa493f5c15853f9e7ea8cde046a4ebea
Charlie Lyons <charlielyons@Kelly-Lyonss-iMac.local>
2023-04-25 22:13:40 -0500

    Testing styled components and display Image on product details

--- src/product-details/ProductDetails.js
+++ src/product-details/ProductDetails.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import backendClient from "../client/backendClient";
+import { Container, Typography, CardMedia, Box } from "@mui/material";
+import styled from "styled-components";
+
+const TitleText = styled.h1`
+  color: palevioletred;
+`;
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -8,18 +14,35 @@ const ProductDetails = () => {
 
   useEffect(() => {
     backendClient.getProductDetailsById(id, (data) => {
-      setProductInfo(data)
+      setProductInfo(data);
     });
   }, [setProductInfo]);
 
-  return <div>
-      <h1>Product: {productInfo.name}</h1>
-      <h2>Price: {productInfo.price}</h2>
-      <h2>Size: {productInfo.size}</h2>
-      <h2>Quantity: {productInfo.quantity}</h2>
-      <h2>Image URL: {productInfo.image_url}</h2>
-      <p>Description: {productInfo.description}</p>
-    </div>;
+  return (
+    <Container sx={{ display: "flex", flexDirection: "row" }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardMedia
+          component="img"
+          image={productInfo.image_url}
+          sx={{
+            width: "100%",
+            padding: "10px",
+          }}
+        ></CardMedia>
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <TitleText>
+          <Typography variant="h2">{productInfo.name}</Typography>
+        </TitleText>
+        <Typography variant="h3">Price: {productInfo.price}</Typography>
+        <Typography variant="h3">Size: {productInfo.size}</Typography>
+        <Typography variant="h3">Quantity: {productInfo.quantity}</Typography>
+        <Typography variant="p">
+          Description: {productInfo.description}
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
 
 export default ProductDetails;
